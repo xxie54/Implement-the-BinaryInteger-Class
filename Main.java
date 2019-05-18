4a57ba3315bf33d8f2fce3a72630c7cc7b7b73d8
xxie54 <47068487+xxie54@users.noreply.github.com>
2019-05-18 16:23:59 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class void Main implements BinaryInteger< >{
+	
+int a =4134235345;
+int b =243252352;
+
+String repString = a.toString()  
+int length= a.length();
+BinaryInteger flipped= a.not();
+int numZero = a.leadingZeros();
+boolean torf = a.isGreaterThan(BinaryInteger b);
+
+System.out.println(repStirng,length, flipped, numZero,torf);
+
+}
